73f2562a04e996663b572b65aa2dcdc8ba44e690
opit <opit@hardcore.lt>
2018-03-29 21:16:18 +0300

    before interrupt handling

--- osc/osc.ino
+++ osc/osc.ino
@@ -1,9 +1,8 @@
 #include <SPI.h>
 #include <SD.h>
-#include <SdFat.h>
-#include "MPU6050.h"
+#include "MPU6050_6Axis_MotionApps20.h"
 #include "I2Cdev.h"
-
+#include "helper_3dmath.h"
 // Arduino Wire library is required if I2Cdev I2CDEV_ARDUINO_WIRE implementation
 // is used in I2Cdev.h
 #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
@@ -15,16 +14,14 @@ int t = 0;
 
 File dataFile;
 
-
-
-
-
 // class default I2C address is 0x68
 // specific I2C addresses may be passed as a parameter here
 // AD0 low = 0x68 (default for InvenSense evaluation board)
 // AD0 high = 0x69
 //MPU6050 accelgyro;
-MPU6050 accelgyro(0x69); // <-- use for AD0 high
+
+MPU6050 mpu(0x69);
+//MPU6050 accelgyro(0x69); // <-- use for AD0 high
 
 int16_t ax, ay, az;
 int16_t gx, gy, gz;
@@ -33,22 +30,39 @@ uint32_t logLength = 30000L;
 
 const int chipSelect = 10;
 
+#define OUTPUT_READABLE_WORLDACCEL;
+#define INTERRUPT_PIN 2
 
-// uncomment "OUTPUT_READABLE_ACCELGYRO" if you want to see a tab-separated
-// list of the accel X/Y/Z and then gyro X/Y/Z values in decimal. Easy to read,
-// not so easy to parse, and slow(er) over UART.
-#define OUTPUT_READABLE_ACCELGYRO
+// MPU control/status vars
+bool dmpReady = false;  // set true if DMP init was successful
+uint8_t mpuIntStatus;   // holds actual interrupt status byte from MPU
+uint8_t devStatus;      // return status after each device operation (0 = success, !0 = error)
+uint16_t packetSize;    // expected DMP packet size (default is 42 bytes)
+uint16_t fifoCount;     // count of all bytes currently in FIFO
+uint8_t fifoBuffer[64]; // FIFO storage buffer
 
-// uncomment "OUTPUT_BINARY_ACCELGYRO" to send all 6 axes of data as 16-bit
-// binary, one right after the other. This is very fast (as fast as possible
-// without compression or data loss), and easy to parse, but impossible to read
-// for a human.
-//#define OUTPUT_BINARY_ACCELGYRO
+// orientation/motion vars
+Quaternion q;           // [w, x, y, z]         quaternion container
+VectorInt16 aa;         // [x, y, z]            accel sensor measurements
+VectorInt16 aaReal;     // [x, y, z]            gravity-free accel sensor measurements
+VectorInt16 aaWorld;    // [x, y, z]            world-frame accel sensor measurements
+VectorFloat gravity;    // [x, y, z]            gravity vector
+float euler[3];         // [psi, theta, phi]    Euler angle container
+float ypr[3]; // [yaw, pitch, roll]   yaw/pitch/roll container and gravity vector
 
 unsigned long previousMillis = 0;        // will store last time of measurement
 unsigned long interval = 10;           // interval at which to measure (milliseconds)
 
-#define LED_PIN 13
+// ================================================================
+// ===               INTERRUPT DETECTION ROUTINE                ===
+// ================================================================
+
+volatile bool mpuInterrupt = false;     // indicates whether MPU interrupt pin has gone high
+void dmpDataReady() {
+    mpuInterrupt = true;
+}
+
+#define LED_PIN 13;
 bool blinkState = false;
 
 void setup() {
@@ -80,6 +94,7 @@ dataFile.flush();
     // join I2C bus (I2Cdev library doesn't do this automatically)
     #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
         Wire.begin();
+        Wire.setClock(400000); // 400kHz I2C clock. Comment this line if having compilation difficulties
     #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
         Fastwire::setup(400, true);
     #endif
@@ -89,27 +104,62 @@ dataFile.flush();
     // it's really up to you depending on your project)
     Serial.begin(38400);
 
- 
+     // initialize device
+    Serial.println(F("Initializing I2C devices..."));
+    mpu.initialize();
+pinMode(INTERRUPT_PIN, INPUT);
 
-    // initialize device
-    Serial.println("Initializing I2C devices...");
-    accelgyro.initialize();
+// verify connection
+    Serial.println(F("Testing device connections..."));
+    Serial.println(mpu.testConnection() ? F("MPU6050 connection successful") : F("MPU6050 connection failed"));
 
-    // verify connection
-    Serial.println("Testing device connections...");
-    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+    // wait for ready
+    Serial.println(F("\nSend any character to begin DMP programming and demo: "));
+    while (Serial.available() && Serial.read()); // empty buffer
+    while (!Serial.available());                 // wait for data
+    while (Serial.available() && Serial.read()); // empty buffer again
 
-    // use the code below to change accel/gyro offset values
-   
+    // load and configure the DMP
+    Serial.println(F("Initializing DMP..."));
+    devStatus = mpu.dmpInitialize();
 
-    // configure Arduino LED pin for output
-    pinMode(LED_PIN, OUTPUT);
+// make sure it worked (returns 0 if so)
+    if (devStatus == 0) {
+        // turn on the DMP, now that it's ready
+        Serial.println(F("Enabling DMP..."));
+        mpu.setDMPEnabled(true);
+
+        // enable Arduino interrupt detection
+        Serial.println(F("Enabling interrupt detection (Arduino external interrupt 0)..."));
+        attachInterrupt(digitalPinToInterrupt(INTERRUPT_PIN), dmpDataReady, RISING);
+        mpuIntStatus = mpu.getIntStatus();
+
+        // set our DMP Ready flag so the main loop() function knows it's okay to use it
+        Serial.println(F("DMP ready! Waiting for first interrupt..."));
+        dmpReady = true;
+
+        // get expected DMP packet size for later comparison
+        packetSize = mpu.dmpGetFIFOPacketSize();
+    } else {
+        // ERROR!
+        // 1 = initial memory load failed
+        // 2 = DMP configuration updates failed
+        // (if it's going to break, usually the code will be 1)
+        Serial.print(F("DMP Initialization failed (code "));
+        Serial.print(devStatus);
+        Serial.println(F(")"));
 }
 
 
+}
+
+// ================================================================
+// ===                    MAIN PROGRAM LOOP                     ===
+// ================================================================
+
 
 void loop() {
-
+if (!dmpReady) return;
 
   Serial.print("Starting data logging");
   for( uint32_t tStart = millis();  (millis()-tStart) < logLength;  ){
@@ -118,29 +168,35 @@ void loop() {
     if(currentMillis - previousMillis > interval) {
       previousMillis = currentMillis;
         Serial.println(currentMillis);
-        // read raw accel/gyro measurements from device
-      accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
-        // if the file is available, write to it:
+                #ifdef OUTPUT_READABLE_WORLDACCEL
+            // display initial world-frame acceleration, adjusted to remove gravity
+            // and rotated based on known orientation from quaternion
+            mpu.dmpGetQuaternion(&q, fifoBuffer);
+            mpu.dmpGetAccel(&aa, fifoBuffer);
+            mpu.dmpGetGravity(&gravity, &q);
+            mpu.dmpGetLinearAccel(&aaReal, &aa, &gravity);
+            mpu.dmpGetLinearAccelInWorld(&aaWorld, &aaReal, &q);
+            Serial.print("aworld\t");
+            Serial.print(aaWorld.x);
+            Serial.print("\t");
+            Serial.print(aaWorld.y);
+            Serial.print("\t");
+            Serial.println(aaWorld.z);
+          #endif
      
         if (dataFile) {
           dataFile.print(millis());
           dataFile.print(",");
-          dataFile.print(ax);
+          dataFile.print(aaWorld.x);
           dataFile.print(",");
-          dataFile.print(ay);
+          dataFile.print(aaWorld.y);
           dataFile.print(",");
-          dataFile.print(az);
-          dataFile.print(",");
-          dataFile.print(gx);
-          dataFile.print(",");
-          dataFile.print(gy);
-          dataFile.print(",");
-          dataFile.print(gz);
+          dataFile.print(aaWorld.z);
           dataFile.println("");
     }
       // if the file isn't open, pop up an error:
       else {
-        Serial.println("error opening datalog.txt");
+        Serial.println("error opening file");
       }
     
     }
