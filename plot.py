4d448ab84bcf78940c5187b1ba13b10b74e8d608
opit <opit@hardcore.lt>
2018-04-24 14:50:26 +0300

    plotting, before removing extra panels

--- plot.py
+++ plot.py
@@ -3,13 +3,13 @@ import numpy as np
 import math
 from scipy import signal
 from wavelets import WaveletAnalysis
-data = np.genfromtxt("output.csv", delimiter=",")
-time = (data[:, 0])/1000000
+data = np.genfromtxt("testas.txt", delimiter=",")
+time = (data[:, 0])/1000
 
 
 x = 9.8*(data[:, 1])/2048
 y = 9.8*(data[:, 2])/2048
-z = 9.8*((data[:, 3]))/2048 # -- factory offset of this particular accelerator!
+z = 9.8*((data[:, 3]) + 430)/2048 # -- factory offset of this particular accelerator!
 
 #x[np.where(np.abs(x) > 5)] = 0
 #y[np.where(np.abs(y) > 5)] = 0
@@ -21,7 +21,7 @@ raw_acc = np.sqrt(x**2 + y**2 + z**2)
 #print np.mean(x), np.mean(y), np.mean(z)
 #exit()
 
-dt = 0.00009
+dt = 0.005
 print(np.mean(np.diff(time)))
 
 vel = np.cumsum(raw_acc * dt) 
@@ -36,22 +36,22 @@ ax[0].legend()
 ax[1].plot(time, z, c="b", label="z")
 ax[1].plot(time, y, c="g", label="y")
 ax[1].plot(time, x, c="r", label="x")
-ax[1].legend()
+ax[1].legend(loc=3)
 ax[2].hist(np.diff(time), bins=20)#plot(time, dist, c="k")
 
-fft = np.abs(np.fft.fft(z))/(2*len(raw_acc))
-freq = np.fft.fftfreq(len(raw_acc), dt)
+fft = np.abs(np.fft.fft(z))/(2*len(z))
+freq = np.fft.fftfreq(len(z), dt)
 
-ax[3].plot(freq, fft, c="k", label="FFT")
+ax[3].plot(freq, fft, c="b", label="FFT")
 
 #f, t, Sxx = signal.spectrogram(x, 100)
 #ax[3].pcolormesh(t, f, Sxx)
-ax[3].axis([0, 100, 0, 1])
+ax[3].axis([0, 100, 0, 0.1])
 ax[3].legend(loc=2)
 plt.xlabel("Hz")
 plt.tight_layout()
 plt.savefig("img/spectrogram.png")
-
+exit()
 fig = plt.figure()
 wa = WaveletAnalysis(raw_acc, dt=dt)
 
@@ -62,7 +62,7 @@ scales = wa.scales
 # associated time vector
 t = wa.time
 T, S = np.meshgrid(time, scales)
-d = plt.contourf(T, S, power, 1000)
+d = plt.contourf(T, S, power, 400)
 plt.colorbar(d)
 plt.xlabel("Time, s")
 plt.ylabel("Scale, s")
