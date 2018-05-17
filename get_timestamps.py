eeac11825a579716bb26597bf211909de6aaeb2e
opit <opit@hardcore.lt>
2018-05-17 13:28:49 +0300

    tidier seconds calculation

--- get_timestamps.py
+++ get_timestamps.py
@@ -11,23 +11,24 @@ def utc_to_local(utc_t):
 input_file = sys.argv[1]
 
 data = np.genfromtxt(input_file, skip_header = 1, delimiter=",", dtype = int)#[132*200:133*200]
-time = data[:, 0]
+seconds = data[:, 0]
+
 x = 9.8*(data[:, 1])/2048
 y = 9.8*(data[:, 2])/2048
 z = 9.8*(data[:, 3] - 150)/2048
 
 with open(input_file) as f:
-    start_time = utc_to_local(datetime.strptime(f.readline().strip(), '%Y-%m-%d %H:%M:%S.%f'))
+    start_time = datetime.strptime(f.readline().strip(), '%Y-%m-%d %H:%M:%S.%f')
+    real_time = utc_to_local(start_time)
+    output_filename = real_time.strftime("%Y%m%d-%H_%M_%S")
 
-output_filename = start_time.strftime("%Y%m%d-%H_%M_%S")
-
-start_offset = time - time[0]
+start_offset = seconds - seconds[0]
 print(x[1])
 timestamps = np.datetime64(start_time) + start_offset*1000 #microseconds
-print(np.datetime64(start_time))
-data = np.zeros(timestamps.size, dtype=[('time', 'U028'), ('x', float),  ('y', float),  ('z', float)])
+data = np.zeros(timestamps.size, dtype=[('time', 'U028'), ('seconds', float), ('x', float),  ('y', float),  ('z', float)])
 data['time'] = timestamps
+data['seconds'] = seconds
 data['x'] = x
 data['y'] = y
 data['z'] = z
-np.savetxt('data/'+output_filename+'_timestamps.csv', data, delimiter = ',', fmt="%s28 %.12f %.12f %.12f")
+np.savetxt('data/'+output_filename+'_timestamps.csv', data, delimiter = ',', fmt="%s28, %i, %.12f, %.12f, %.12f")
